da7c9d66c801f1fea949db481068c036727e1273
shihanspatel <167397702+shihanspatel@users.noreply.github.com>
2026-03-20 10:57:46 +0530

    increase the height of the map.

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -237,7 +237,7 @@
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.5054701234567!2d70.80217!3d22.30389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959ca248b7d5555%3A0x1234567890abcdef!2sRajkot%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1234567890"
                     width="100%"
-                    height="500"
+                    height="600"
                     style="border:0;"
                     allowfullscreen=""
                     loading="lazy">
